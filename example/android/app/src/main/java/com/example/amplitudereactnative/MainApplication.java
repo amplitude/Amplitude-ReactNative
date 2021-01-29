504239408da2d15801ad8670dd016106e216e737
Kelson Warner <kelson.warner@amplitude.com>
2021-01-28 23:03:52 -0800

    Add final fixes so android bridging works

--- example/android/app/src/main/java/com/example/amplitudereactnative/MainApplication.java
+++ example/android/app/src/main/java/com/example/amplitudereactnative/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.amplitudereactnative.ReactNativePackage;
+import com.amplitude.reactnative.AmplitudeReactNativePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for ReactNativeExample:
           // packages.add(new MyReactNativePackage());
-          packages.add(new ReactNativePackage());
+          packages.add(new AmplitudeReactNativePackage());
           return packages;
         }
 
