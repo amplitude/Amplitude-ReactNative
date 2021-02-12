b50f7c5294479046d01e7790fa8c77b0541ea8d6
Kelson Warner <kelson.warner@amplitude.com>
2021-02-11 16:54:24 -0800

    Add underscore to logEvent, initialize, and add logEvent code to example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
+
+import { Amplitude } from '@amplitude/react-native';
+
+const instance = Amplitude.getInstance();
+instance.init('9c9566b48da3f559498fd70b164303b8');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Please Build me!</Text>
+      <Button
+        title={'Log an event!'}
+        onPress={() => {
+          Amplitude.getInstance()
+            .logEvent('Test Event 2000')
+            .then((res) => {
+              console.warn({ res });
+            });
+        }}
+      />
     </View>
   );
 }
