3fab73a190eb8e5842990657fbf73ec33d2fd452
Kelson Warner <kelson.warner@amplitude.com>
2021-02-22 15:36:17 -0800

    Add flush events button

--- example/src/SdkMethodSections/FlushEventsSection.tsx
+++ example/src/SdkMethodSections/FlushEventsSection.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-// import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+import { amplitudeInstance } from '../utils/amplitude';
 
 import { SdkSectionLayout } from './SdkSectionLayout';
 
 export const FlushEventsSection = () => {
   return (
     <SdkSectionLayout heading={'Flush Events'}>
-      {/* UI for flushing events goes here */}
+      <Button
+        title={'Click to Flush Events'}
+        onPress={() => {
+          amplitudeInstance.uploadEvents();
+        }}
+      />
     </SdkSectionLayout>
   );
 };
-
-// const styles = StyleSheet.create({});
