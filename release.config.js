4890e3586e3e92f85f06709a2928e8926712eef6
yuhao900914 <yuhao900914@gmail.com>
2021-03-11 22:05:55 -0800

    ci: add release ci & fix doc deploy & change npm whitelist

---
+++ release.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+      },
+    ],
+    ['@semantic-release/github'],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['src/index.ts', 'docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
